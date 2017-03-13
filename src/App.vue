60d211f6873a79858154f649bf4c905761bbadd3
Kristoffer Darj <kristoffer.darj@gmail.com>
2017-03-13 23:16:16 +0100

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <app></app>
     <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
--- src/main.js
+++ src/main.js
@@ -10,16 +10,20 @@ import Hello from './components/Hello';
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
+const Foo = { template: '<div>foo</div>' };
+const Bar = { template: '<div>bar</div>' };
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar },
+  { path: '/hello', component: Hello },
+];
+
 const router = new VueRouter({
-  routes: [
-    {
-      path: '/hello',
-      component: Hello,
-    },
-  ],
+  routes,
 });
 
-// Vue.config.productionTip = false;
+Vue.config.debug = true;
 
 /* eslint-disable no-new, no-unused-vars */
 const app = new Vue({
