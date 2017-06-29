adbaf386388b1c6e60220f357fda01913ab9bbfb
Kristoffer Darj <kristoffer.darj@gmail.com>
2017-06-29 22:08:47 +0200

    Toolbar styling

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <nav class="mdc-toolbar mdc-toolbar--fixed mdc-toolbar--waterfall" data-mdc-auto-init="MDCIconToggle">
       <div class="mdc-toolbar__row">
         <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
-          <a href="#" class="material-icons mdc-toolbar__icon--menu">menu</a>
           <router-link to="/blogroll" class="mdc-toolbar__title">Kristoffer Darj</router-link>
         </section>
       </div>
@@ -24,3 +23,13 @@ export default {
   },
 };
 </script>
+<style scoped>
+.mdc-toolbar {
+  background-color: #263238;
+  color: #FFF;
+}
+.mdc-toolbar__title {
+  color: #FFF;
+  text-decoration: none;
+}
+</style>
